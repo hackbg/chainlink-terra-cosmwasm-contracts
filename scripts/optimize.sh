6a583caf77ccca36941f2d3546bd4fdb3853184b
Nikolay Zaharov <nzaharov988@gmail.com>
2021-03-16 22:11:29 +0200

    build: added script for wasm optimization

---
+++ scripts/optimize.sh
@@ -0,0 +1,5 @@
+# Optimizes wasm contract build output
+docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/rust-optimizer:0.10.3
