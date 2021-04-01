dcf54ff956f55fb67dd817ce97936efe3bc825ab
Nikolay Zaharov <nzaharov988@gmail.com>
2021-04-01 23:19:09 +0300

    feat: initial messages defined

--- contracts/flux-aggregator/examples/schema.rs
+++ contracts/flux-aggregator/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use flux_aggregator::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use flux_aggregator::msg::{HandleMsg, InitMsg, QueryMsg};
 use flux_aggregator::state::State;
 
 fn main() {
@@ -16,5 +16,4 @@ fn main() {
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
 }
--- contracts/flux-aggregator/src/msg.rs
+++ contracts/flux-aggregator/src/msg.rs
@@ -1,27 +1,111 @@
+use cosmwasm_std::{HumanAddr, Uint128};
+use cw20::Cw20ReceiveMsg;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    pub link: HumanAddr,
+    pub payment_amount: Uint128,
+    pub timeout: u32,
+    pub validator: HumanAddr,
+    pub min_submission_value: i128, // TODO: Might have to bump to BigInt
+    pub max_submission_value: i128,
+    pub decimals: u8,
+    pub description: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    Submit {
+        round_id: Uint128,
+        submission: i128,
+    },
+    ChangeOracles {
+        removed: Vec<HumanAddr>,
+        added: Vec<HumanAddr>,
+        added_admins: Vec<HumanAddr>,
+        min_submissions: u32,
+        max_submissions: u32,
+        restart_delay: u32,
+    },
+    WithdrawPayment {
+        oracle: HumanAddr,
+        recipient: HumanAddr,
+        amount: Uint128,
+    },
+    WithdrawFunds {
+        recipient: HumanAddr,
+        amount: Uint128,
+    },
+    TransferAdmin {
+        oracle: HumanAddr,
+        new_admin: HumanAddr,
+    },
+    AcceptAdmin {
+        oracle: HumanAddr,
+    },
+    RequestNewRound {},
+    SetRequesterPermissions {
+        requester: HumanAddr,
+        authorized: bool,
+        delay: u32,
+    },
+    UpdateFutureRounds {
+        payment_amount: Uint128,
+        min_submission: u32,
+        max_submission: u32,
+        restart_delay: u32,
+        timeout: u32,
+    },
+    UpdateAvailableFunds {},
+    SetValidator {
+        validator: HumanAddr,
+    },
+    Receive(Cw20ReceiveMsg),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    GetAllocatedFunds {},
+    GetAvailableFunds {},
+    GetWithdrawablePayment {
+        oracle: HumanAddr,
+    },
+    GetOracleCount {},
+    GetOracles {},
+    GetAdmin {
+        oracle: HumanAddr,
+    },
+    GetRoundData {
+        round_id: Uint128,
+    },
+    GetLatestRoundData {},
+    GetOracleRoundState {
+        oracle: HumanAddr,
+        queried_round_id: u32,
+    },
 }
 
-// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct RoundDataResponse {
+    pub round_id: Uint128,          // uint80
+    pub answer: i128,               // int256
+    pub started_at: Uint128,        // int256
+    pub updated_at: Uint128,        // uint256
+    pub answered_in_round: Uint128, // uint80
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct OracleRoundStateResponse {
+    pub elegible_to_submit: bool, // bool
+    pub round_id: u32,            // uint32
+    pub latest_submission: i128,  // int256
+    pub started_at: u64,          // uint64
+    pub timeout: u64,             // uint64
+    pub available_funds: Uint128, // uint128
+    pub oracle_count: u8,         // uint8
+    pub payment_amount: Uint128,  // uint128
 }
