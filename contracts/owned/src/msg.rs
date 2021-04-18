08d5419abb130f2fa1b6f3194550e59296fc2146
denis <denismaximov98@gmail.com>
2021-04-18 09:22:08 +0300

    chore: cleanup

--- contracts/owned/src/msg.rs
+++ contracts/owned/src/msg.rs
@@ -15,6 +15,5 @@ pub enum HandleMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
     GetOwner {},
 }
